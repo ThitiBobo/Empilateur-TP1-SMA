ba8c4bdbe25c8459ed6ff8188f82bf0da0389e7e
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-10-11 14:50:55 +0200

    DEV : implémentation de la classe Pile

--- src/main/java/com/example/empilateurtp1sma/Pile.java
+++ src/main/java/com/example/empilateurtp1sma/Pile.java
@@ -1,4 +1,22 @@
 package com.example.empilateurtp1sma;
 
+import java.util.Stack;
+
 public class Pile {
+
+    private Stack stack;
+
+    public Pile(){
+        this.stack = new Stack<Agent>();
+    }
+
+    public void add(Agent agent){
+        this.stack.add(agent);
+    }
+
+    public Agent pop(){
+        this.stack.pop();
+    }
+
 }
+
