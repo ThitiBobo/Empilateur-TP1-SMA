40e3cdf9072acf63d39cb5aa7fc7566e3b107013
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-10-11 15:07:00 +0200

    DEv : init classe Block

--- src/main/java/com/example/empilateurtp1sma/Bloc.java
+++ src/main/java/com/example/empilateurtp1sma/Bloc.java
@@ -1,4 +1,25 @@
 package com.example.empilateurtp1sma;
 
+import org.springframework.cglib.core.Block;
+
 public class Bloc {
+
+    private int id;
+    private Block objectif;
+
+    public Bloc(int id){
+        this.id = id;
+    }
+
+    public void setObjectif(Block objectif){
+        this.objectif = objectif;
+    }
+
+    public void move(){
+        return;
+    }
+
+    public void push(){
+        return;
+    }
 }
