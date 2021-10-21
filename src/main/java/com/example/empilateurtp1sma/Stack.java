6af79109fb79c82080f6f2f6854bc1faf4360389
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-10-21 12:58:50 +0200

    DEV : modification of "Ressources" classes

--- src/main/java/com/example/empilateurtp1sma/Stack.java
+++ src/main/java/com/example/empilateurtp1sma/Stack.java
@@ -3,14 +3,19 @@ package com.example.empilateurtp1sma;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Stack implements Resource{
+public class Stack extends Resource{
 
     private java.util.Stack<Agent> stack;
 
-    public Stack(){
+    public Stack(String tag){
+        super(tag);
         this.stack = new java.util.Stack<>();
     }
 
+    /**
+     * Pushes an {@code Agent} onto the top of this stack.
+     * @param agent, an {@code Agent} to pushes
+     */
     public void push(Agent agent){
         this.stack.push(agent);
     }
@@ -22,6 +27,23 @@ public class Stack implements Resource{
 
     public boolean contains(Agent agent){
         return this.stack.contains(agent);
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "tag='" + tag + '\'' +
+                ", stack=" + stack +
+                '}';
+    }
+
+    public String display(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("stack:" + this.tag);
+        builder.append(" [ ");
+        this.stack.forEach( agent -> builder.append(agent.getTag() + " "));
+        builder.append("]");
+        return builder.toString();
     }
 }
 
